1e4f67066989329d52a5f9e0e3967dc10b58e25a
tmuzikova <tereza.muzikova@gmail.com>
2024-09-16 12:40:59 +0200

    removed unecessary stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter } from "react-router-dom";
 import {
   LanguageProvider,
   useLanguageContext,
@@ -18,34 +17,31 @@ function App() {
 
   return (
     <IntlProvider locale={language} messages={messages[language]}>
-      <BrowserRouter>
-        <div className="relative z-0">
-          <div className="fixed top-0 left-0 w-full z-20">
-            <Navbar />
-          </div>
+      <div className="relative z-0">
+        <div className="fixed top-0 left-0 w-full z-20">
+          <Navbar />
+        </div>
 
-          {/* Hero Section with Light Teal Gradient Overlay */}
-          <div className="relative bg-intro-pattern bg-cover bg-no-repeat bg-center">
-            <div
-              className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-[#481557] z-0"
-              style={{
-                backgroundImage:
-                  "linear-gradient(to bottom, transparent 55%, #481557 100%)",
-              }}
-            />
-            <div className="relative z-10">
-              <Hero />
-            </div>
-          </div>
-
-          <div className="relative z-10 bg-primary">
-            <About />
-            <Works />
-            <Contact />
-            <StarsCanvas />
+        <div className="relative bg-intro-pattern bg-cover bg-no-repeat bg-center">
+          <div
+            className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-[#481557] z-0"
+            style={{
+              backgroundImage:
+                "linear-gradient(to bottom, transparent 55%, #481557 100%)",
+            }}
+          />
+          <div className="relative z-10">
+            <Hero />
           </div>
         </div>
-      </BrowserRouter>
+
+        <div className="relative z-10 bg-primary">
+          <About />
+          <Works />
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
     </IntlProvider>
   );
 }
